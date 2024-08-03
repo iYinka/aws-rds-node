d5f70b6ee9fdb47e56762a055ba9064e412ab1c8
iYinka <olayinkav28@gmail.com>
2024-08-04 00:53:04 +0100

    added db

--- db.js
+++ db.js
@@ -4,18 +4,19 @@ const mysql = require('mysql2/promise')
 const dotenv = require("dotenv");
 dotenv.config();
 
-const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DBNAME,
-    port: process.env.PORT,
-});
-
 // const db = mysql.createPool({
-//     host: "localhost",
-//     user: "admin",
-//     password: "password",
-//     database: "employee_db",
+//     host: process.env.HOST,
+//     user: process.env.USER,
+//     password: process.env.PASSWORD,
+//     database: process.env.DBNAME,
+//     port: process.env.PORT,
 // });
+
+const db = mysql.createPool({
+    host: "employeedb.cx6osya6kdrn.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "password1234",
+    database: "employee_db",
+    port: 3306
+});
 module.exports = db;
